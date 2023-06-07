8efb8a36049bb87b8bf7862e1411389343ca0d53
Egelund48 <79721682+Egelund48@users.noreply.github.com>
2023-06-07 10:01:10 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Hjemmeside prep</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <nav>
+            <div class="navbar" >
+                <li><a href=>Kontakt</a></li>
+                <li><a href=>Om os</a></li>
+            </div>
+        </nav>
+        <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+            <img class="image" src="billeder/pic_1.jpg" draggable="false"/>
+            <img class="image" src="billeder/pic_2.jpg" draggable="false"/>
+            <img class="image" src="billeder/pic_3.jpg" draggable="false"/>
+            <img class="image" src="billeder/pic_4.jpg" draggable="false"/>
+            <img class="image" src="billeder/pic_5.jpg" draggable="false"/>
+            <img class="image" src="billeder/pic_6.jpg" draggable="false"/>
+            <img class="image" src="billeder/pic_7.jpg" draggable="false"/>
+        </div>
+
+        <script type="text/javascript" src="main.js"></script>
+    </body>
+</html>
